6609adf04c2b4d6161598383bc2a51cd3f03613d
Isaac Adams <isaacdadams@pm.me>
2024-10-29 19:31:13 -0400

    updating host

--- test/load.sh
+++ test/load.sh
@@ -1,9 +1,10 @@
+HOST=http://localhost:9050
 
 # Create a dataset
-bq --api http://0.0.0.0:9050 --project_id=test mk test_dataset
+bq --api $HOST --project_id=test mk test_dataset
 
 # Create a table with schema
-bq --api http://0.0.0.0:9050 --project_id=test mk --table test_dataset.test_table name:STRING,age:INTEGER
+bq --api $HOST --project_id=test mk --table test_dataset.test_table name:STRING,age:INTEGER
 
 # load data
-bq --api http://0.0.0.0:9050 --project_id=test query < insert_statements.sql
+bq --api $HOST --project_id=test query < ./test/insert_statements.sql
