6988585a52eab49b647ab3087b37fecc8353e494
Isaac Adams <26559009+isaacadams@users.noreply.github.com>
2024-12-29 16:09:45 -0500

    tests

--- src/parity.rs
+++ src/parity.rs
@@ -9,13 +9,49 @@ const BQ: &str = "bq.cmd";
 #[cfg(not(windows))]
 const BQ: &str = "bq";
 
-#[test]
-fn compare_bq_and_bq_rs_outputs() -> io::Result<()> {
-    // Build the Rust project in release mode
+/// Build the Rust project in release mode
+fn release() -> io::Result<()> {
     let build_status = Command::new("cargo")
         .args(["build", "--release", "-q"])
         .status()?;
     assert!(build_status.success(), "Cargo build failed!");
+    Ok(())
+}
+
+#[test]
+fn should_match_expected_md5() -> io::Result<()> {
+    release()?;
+
+    // Execute the `bq-rs` query
+    let bq_rs_output = Command::new("./target/release/bq-rs")
+        .args([
+            "--api=http://localhost:9050",
+            "--project-id=test",
+            "query",
+            "SELECT * FROM test_dataset.test_table",
+        ])
+        .output()?;
+
+    assert!(
+        bq_rs_output.status.success(),
+        "Failed to run `bq-rs` query: {}",
+        String::from_utf8_lossy(&bq_rs_output.stderr)
+    );
+
+    //md5::compute(bq_rs_output.stdout).as_ref();
+
+    assert_eq!(
+        format!("{:#?}", md5::compute(&bq_rs_output.stdout)).as_str(),
+        "cce3a33e8e72c2ce410c3ef9094800e7",
+        "md5 digest is not correct"
+    );
+
+    Ok(())
+}
+
+#[test]
+fn compare_bq_and_bq_rs_outputs() -> io::Result<()> {
+    release()?;
 
     // Execute the `bq` query
     let bq_output = Command::new(BQ)
